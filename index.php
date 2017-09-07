b221858f8a74ffdface3edc358db0835c9217067
Dominic Fajilan <dfajilan@csumb.edu>
2017-09-07 20:15:22 +0000

    Homework 1

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<!--
+
+First Website
+and comment
+in html
+(comments can span multiple lines)
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset="utf-8" />
+        <title> Operating Systems </title>
+        <link href="css/style.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    </head>
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        <header>
+            <h1> Operating Systems </h1>
+        </header>
+            <nav>
+                <a href="index.php" id="home">Home</a>
+                <a href="windows.html">Windows</a>
+                <a href="linux.html">Linux</a>
+                <a href="macOS.html">MacOS</a>
+            </nav>
+            
+            <br /><br />
+            
+            <main>
+                
+                <h2>Operating Systems</h2>
+                <figure id="OS">
+                    <img src="img/operatingsystems.jpg" alt="Picture of Operating Systems" />
+                </figure>
+                
+                <div id="welcomeText">
+                    An operating system (OS) is system software that manages computer <br />
+                    hardware and software resources and provides common services <br />
+                    for computer programs. All computer programs, excluding firmware, <br />
+                    require an operating system to function. <br /> <br /> 
+                    
+                    There are three main types of operating systems that dominate <br />
+                    desktop operating systems: Microsoft Windows, MacOS by Apple <br />
+                    Inc., and the varieties of Linux. <br /> <br />
+                    
+                    <em>
+                        <a href="https://en.wikipedia.org/wiki/Operating_system" id="source">source</a>
+                    </em>
+                </div>
+                
+            </main>
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
